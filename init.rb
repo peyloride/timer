c5f76c74f3a8c17a156e35521d038996dfc48f41
Okan Binli <okanb3@gmail.com>
2016-05-15 17:08:06 +0300

    some small improvements

--- init.rb
+++ init.rb
@@ -3,7 +3,7 @@ Redmine::Plugin.register :timer do
   author 'Okan Binli'
   description 'This is a plugin for display which user works on which issue how many hours by Chart.js pie charts.'
   version '0.0.1'
-  url 'http://github.com'
+  url 'https://github.com/peyloride/timer/'
   author_url 'http://peyloride.xyz'
 
   project_module :timer do
